5abc50a69bbd4369eb0baa805eb025ae11fee1e3
AlessioConti <alessioconti02@yahoo.it>
2022-06-10 17:10:52 +0200

    Implementazione insert Satellite base

--- src/main/java/it/prova/gestionesatelliti/web/controller/SatelliteController.java
+++ src/main/java/it/prova/gestionesatelliti/web/controller/SatelliteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,6 +26,17 @@ public class SatelliteController {
 		mv.addObject("satellite_list_attribute", results);
 		mv.setViewName("satellite/list");
 		return mv;
+	}
+	
+	@GetMapping("/search")
+	public String search() {
+		return "satellite/search";
+	}
+	
+	@GetMapping("/insert")
+	public String create(Model model) {
+		model.addAttribute("insert_satellite_attr", new Satellite());
+		return "satellite/insert";
 	}
 	
 }
